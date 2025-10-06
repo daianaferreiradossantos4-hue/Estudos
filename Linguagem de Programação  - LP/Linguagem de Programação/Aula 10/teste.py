75f8a064f09bc1cad4fe0a2affff54ea6f1be604
daianaferreiradossantos4-hue <daianaferreiradossantos4@gmail.com>
2025-10-06 19:03:16 -0300

    alteracao 06.10

---
+++ Linguagem de Programação  - LP/Linguagem de Programação/Aula 10/teste.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time 
+#mouse
+print(pyautogui.position())
+
+#while(True):#loop infinito
+
+print(pyautogui.position())
+pyautogui.moveTo(1280, 60,duration= 2)#movimentar o mouse 
+pyautogui.moveTo(200, 600,duration= 2)
+pyautogui.moveTo(700,70,duration= 2)
+pyautogui.moveTo(3,87,duration= 2)
